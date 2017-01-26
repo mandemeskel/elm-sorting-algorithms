c49d2154957f70ff66f90a58705a57d8f254bf60
Michael T. Andemeskel <mtandemeskel@gmail.com>
2017-01-26 11:07:17 -0800

    created generic getSubArray function

--- merge/mergesort.elm
+++ merge/mergesort.elm
@@ -58,6 +58,40 @@ splitArray a =
     ( firstHalf a, lastHalf a )
 
 
+
+-- Int a and Int b causes an error, elm expects an atom type?
+-- getSubArray : List a -> Int a -> Int b -> List a
+{--
+Your type annotation uses `d` and `a` as separate type variables. Each one
+can be any type of value, but because they have separate names, there is no
+guarantee that they are equal to each other. Your code seems to be saying they
+ARE the same though! Maybe they should be the same in your type annotation?
+Maybe your code uses them in a weird way?
+--}
+-- getSubArray : List a -> Int -> Int -> List d
+{--
+Get sub array of an array from start index, inclusive, to stop index, exclusive.
+--}
+
+
+getSubArray : List a -> Int -> Int -> List a
+getSubArray array start stop =
+    -- List.reverse (List.drop stop (List.reverse (List.drop start array)))
+    -- let
+    --   if start > 0 then
+    --     n =
+    --         start - 1
+    --   else
+    --     n = start
+    -- in
+    --     (List.drop n array)
+    -- if start > 0 then
+    -- List.take (stop - start) (List.drop start array)
+    -- else
+    -- List.take (stop - start) (List.drop start array)
+    List.take (stop - start) (List.drop start array)
+
+
 mergeArrays aleft aright =
     []
 
@@ -79,9 +113,29 @@ mergeSort a =
         a
 
 
+array =
+    [ 1, 2, 3, 4, 5, 6 ]
+
+
+start =
+    -- List.length array // 2
+    -- 0
+    2
+
+
+stop =
+    -- List.length array
+    -- List.length array // 2
+    5
+
+
 result =
-    splitArray [ 1, 2, 4, 5, 6 ]
+    getSubArray array start stop
+
+
+
+-- use ++ to concat strings
 
 
 main =
-    Html.text (toString result)
+    Html.text ((toString result) ++ " " ++ (toString array))

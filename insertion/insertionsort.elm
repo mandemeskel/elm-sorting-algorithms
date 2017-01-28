5f2959bd38b02e70300750798704c5d6bdcb83ea
Michael T. Andemeskel <mtandemeskel@gmail.com>
2017-01-28 01:18:36 -0800

    started work insertion sort

---
+++ insertion/insertionsort.elm
@@ -0,0 +1,12 @@
+module Main exposing (..)
+
+import Html
+import List
+
+
+-- NOTE: comparable is a type that can be compared i.e. Int, String, Float
+
+
+insertionSort : List comparable -> List comparable
+insertionSort array =
+    array

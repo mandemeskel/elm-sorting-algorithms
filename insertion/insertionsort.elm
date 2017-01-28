f21bd8481c653d9ab3e8d8de8eb217b0a04291a8
Michael T. Andemeskel <mtandemeskel@gmail.com>
2017-01-28 05:00:41 -0800

    some initial work

--- insertion/insertionsort.elm
+++ insertion/insertionsort.elm
@@ -8,5 +8,42 @@ import List
 
 
 insertionSort : List comparable -> List comparable
-insertionSort array =
-    array
+insertionSort lst =
+    if List.length lst == 0 then
+        []
+    else if List.length lst == 1 then
+        lst
+    else
+        let
+            n1 =
+                [ List.head lst ]
+
+            tail_lst =
+                List.tail lst
+
+            n2 =
+                insertionSort tail_lst
+        in
+            if List.length n2 == 0 then
+                n1
+            else
+                mergeList n1 n2
+
+
+mergeList lst1 list2 =
+    case list1 of
+        [] ->
+            if List.length list2 == 0 then
+              []
+            else
+              [ list2[0] ]
+        [ x ] ->
+            if List.length list2 == 0 then
+                [ x ]
+            else if x < list2 [ 0 ] then
+                [ x ]
+            else
+                [ list2 [ 0 ] ]
+        x::y ->
+          let
+            merged mergeList 
